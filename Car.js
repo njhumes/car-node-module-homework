8f0ffc1a15d4d997c11a2add941f04dff80fd2cf
Nicholas Humes <njhumes@gmail.com>
2019-01-11 16:16:04 -0700

    HW complete

--- Car.js
+++ Car.js
@@ -1,5 +1,15 @@
 class Car{
-    
+    constructor(color, convertible){
+        this.color = color;
+        this.convertible = convertible;
+        this.speed = 0;
+    }
+    accelerate(speed) {
+        this.speed = this.speed + speed;
+    }
+    decelerate(speed) {
+        this.speed = this.speed - speed;
+    }
 }
 
 module.exports = Car;
